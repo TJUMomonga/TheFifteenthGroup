68ed1f1e94665077d181898fec81d5daf4cb54cb
heyang <493763032@qq.com>
2017-10-24 20:41:51 +0800

    data produce

--- CAN信号测试用例/produceTestCase.cpp
+++ CAN信号测试用例/produceTestCase.cpp
@@ -1,2 +1,19 @@
+#include <fstream>
 #include <cstdio>
+#include <cstring>
 
+using namespace std;
+
+int main(){
+    ifstream fin("data.in");
+    ofstream fout("data.txt");
+    
+    string mes;
+    int num = 1;
+    
+    while(getline(fin,mes)){
+    	string a = mes.substr(0,3);
+	}
+    
+    return 0;
+}
